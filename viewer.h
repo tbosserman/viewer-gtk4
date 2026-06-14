b5d5fb6a13f2c434d7298e7329431fefb5430735
Tim Bosserman <tim.bosserman@gmail.com>
2026-06-13 22:07:42 -0500

    Officially declaring version 1.0

--- viewer.h
+++ viewer.h
@@ -3,7 +3,7 @@
 # define TRUE		1
 #endif
 #define MAX_FILES	2000
-#define VERSION		"0.3alpha"
+#define VERSION		"1.0"
 #define DEFAULT_UI_FILE	"Viewer-gtk4.ui"
 #define DEFAULT_GEOMETRY "1024x768"
 #define PREV		0
